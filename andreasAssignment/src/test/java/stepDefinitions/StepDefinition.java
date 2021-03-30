7fe6a9e30fb0270d999234949d15d263343588c6
Kjell <77965179+andreaskjell@users.noreply.github.com>
2021-03-30 15:44:07 +0200

    Changed thread to 4000
    Created feature file
    
    Created TestRunner file
    
    Created empty StepDefinitions

--- andreasAssignment/src/test/java/stepDefinitions/StepDefinition.java
+++ andreasAssignment/src/test/java/stepDefinitions/StepDefinition.java
@@ -1,33 +1,32 @@
 package stepDefinitions;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import java.util.UUID;
 
-public class StepDefinition {
+
+public class StepDefinition {	
 	
-	
-	
-	
-	
-	
-	@Given("I have logged in at Mailchimp")
-	public void i_have_logged_in_at_mailchimp() {
+	@Given("I have opened the Mailchimp login page")
+	public void i_have_opened_the_Mailchimp_login_page() {
 		
-		System.out.println("log in");	
+			
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kjell\\eclipse\\java-2020-12\\eclipse\\addons\\chromedriver.exe");
-
 	    WebDriver driver = new ChromeDriver();
 	    driver.manage().window().maximize();
 	    driver.get("https://login.mailchimp.com/signup/");
-	    driver.quit();
 	}
 	@Given ("I have entered {string}")
 	public void i_have_entered(String email) {
 		System.out.println("Email" +email);
+		
 		
 	}
 	@Given ("i check for the {int} in step")
--- andreasAssignment/src/test/resources/Features/createAccount.feature
+++ andreasAssignment/src/test/resources/Features/createAccount.feature
@@ -2,12 +2,12 @@ Feature: Create Account
  I want to use this feature to test the login function of a website.
 
   Scenario Outline: Title of your scenario outline
-    Given I have logged in at Mailchimp
-    And i have entered <email>
+    Given I have opened the Mailchimp login page
+   And i have entered <email>
     When I check for the <value> in step
     Then I verify the <status> in step
 
     Examples: 
       | email  | value | status  |
-      | name1 |     5 | success |
-      | name2 |     7 | Fail    |
+      | "andrkjell@giganten.se" |     5 | success |
+      | "test@test.se" |     7 | Fail    |
